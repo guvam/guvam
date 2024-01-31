b240c4e836d3ad4c1a0fa35d8c1875916eb5c443
Chankso <chanksoitc@gmail.com>
2024-01-31 15:10:13 +0400

    fixed issues

--- packages/website/src/components/Clipboard.ts
+++ packages/website/src/components/Clipboard.ts
@@ -4,8 +4,10 @@ import { property, customElement } from 'lit/decorators.js';
 @customElement('started-clipboard')
 export class Clipboard extends LitElement {
   // Attribute in parent element should be lowercase
-  // eg copystr = npm i @guvam/guvam
-  @property({ type: String }) copyStr = 'default';
+  // eg copystr ="npm i @guvam/guvam"
+  @property({ type: String }) Value = '';
+
+  @property() timeout: NodeJS.Timeout | null = null;
 
   connectedCallback(): void {
     super.connectedCallback();
@@ -21,67 +23,12 @@ export class Clipboard extends LitElement {
 
   private _copyToClipboard() {
     this.classList.add('Started-Copied');
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+    this.timeout = setTimeout(() => this.classList.remove('Started-Copied'), 1000);
 
-    setTimeout(() => this.classList.remove('Started-Copied'), 1000);
-
-    void navigator.clipboard.writeText(this.copyStr).catch(() => {});
+    void navigator.clipboard.writeText(this.Value).catch(() => {});
   }
 }
 customElements.define('started-clipboard', Clipboard);
-
-/* static count
-
-@customElement('started-clipboard')
-export class Clipboard extends LitElement {
-  static count = 0;
-
-  connectedCallback(): void {
-    super.connectedCallback();
-    this.addEventListener('click', (e) => {
-      console.log('callback fired', Clipboard.count);
-    });
-  }
-
-  protected render() {
-    return html`
-      <p><button @click="${Clipboard._increment}">Click Me!</button></p>
-      <p>Click count: ${Clipboard.count}</p>
-    `;
-  }
-
-  private _increment(e: Event) {
-    Clipboard.count++;
-  }
-}
-
-
-
-*/
-
-/* property count
-
-
-export class Clipboard extends LitElement {
-  @property() count = 0;
-
-  connectedCallback(): void {
-    super.connectedCallback();
-    this.addEventListener('click', (e) => {
-      console.log('callback fired', this.count);
-    });
-  }
-
-  protected render() {
-    return html`
-      <p><button @click="${this._increment}">Click Me!</button></p>
-      <p>Click count: ${this.count}</p>
-    `;
-  }
-
-  private _increment(e: Event) {
-    this.count++;
-  }
-}
-
-
-*/

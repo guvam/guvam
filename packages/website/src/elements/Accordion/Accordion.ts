737e86e7aa84fa6ff7cdd3cf5fa27a278b4dfca9
Chankso <chanksoitc@gmail.com>
2024-02-06 22:18:39 +0400

    Accordion single open behavior

---
+++ packages/website/src/elements/Accordion/Accordion.ts
@@ -0,0 +1,41 @@
+import { LitElement, html } from 'lit';
+import { property, customElement, queryAssignedElements } from 'lit/decorators.js';
+
+@customElement('guvam-accordion')
+export class Accordion extends LitElement {
+  @property({ type: Boolean }) singleOpen = false;
+
+  @queryAssignedElements({ selector: '.Accordion-Item', flatten: false }) accordionItems!: HTMLElement[];
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    console.log(this.singleOpen);
+    this.accordionItems.forEach((el) => {
+      el.addEventListener('click', (e) => {
+        e.stopPropagation();
+        this.toggleOpen(el);
+      });
+    });
+  }
+
+  render() {
+    return html`<slot />`;
+  }
+
+  toggleOpen(el: HTMLElement) {
+    el.classList.toggle('Accordion-Active');
+    if (el.classList.contains('Accordion-Active') && this.singleOpen) {
+      console.log(this.singleOpen);
+      this.closeAllExcept(el);
+    }
+    console.log('toggled');
+  }
+
+  closeAllExcept(el: HTMLElement) {
+    this.accordionItems.forEach((element) => {
+      if (el !== element) {
+        element.classList.remove('Accordion-Active');
+      }
+    });
+  }
+}

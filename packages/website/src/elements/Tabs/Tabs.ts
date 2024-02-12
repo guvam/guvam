243311da85a5dde17cec48618b1b875dfab0a37f
Chankso <chanksoitc@gmail.com>
2024-02-12 12:57:53 +0400

    Tabs restructured

--- packages/website/src/elements/Tabs/Tabs.ts
+++ packages/website/src/elements/Tabs/Tabs.ts
@@ -3,23 +3,22 @@ import { property, customElement, queryAssignedElements } from 'lit/decorators.j
 
 @customElement('guvam-tabs')
 export class Tabs extends LitElement {
-  @queryAssignedElements({ selector: ':not(.Tab-Window)', flatten: true }) headers!: HTMLElement[];
+  @queryAssignedElements({ selector: '[data-target="Tab-Headers"]' }) headersParent!: HTMLElement[];
 
-  @queryAssignedElements({ selector: '.Tab-Window', flatten: true }) tabWindow!: HTMLElement[];
+  @queryAssignedElements({ selector: '[data-target="Tab-Contents"]' }) contentsParent!: HTMLElement[];
 
   connectedCallback(): void {
     super.connectedCallback();
 
-    console.log(this.headers);
-    console.log(this.tabWindow);
+    const headers = this.headersParent[0].children;
 
-    this.headers.forEach((el, index) => {
-      el.addEventListener('click', () => {
-        const contentChild = this.tabWindow[0].children.item(index);
-        this.openTab(contentChild);
+    for (let i = 0; i < headers.length; i++) {
+      headers.item(i)?.addEventListener('click', () => {
+        const content = this.contentsParent[0].children.item(i);
+        this.openTab(content);
         console.log('clicked');
       });
-    });
+    }
   }
 
   render() {
@@ -28,7 +27,7 @@ export class Tabs extends LitElement {
 
   openTab(el: Element | null) {
     if (el) el.classList.add('Tab-Active');
-    const children = this.tabWindow[0].children;
+    const children = this.contentsParent[0].children;
 
     for (let i = 0; i < children.length; i++) {
       const child = children.item(i);

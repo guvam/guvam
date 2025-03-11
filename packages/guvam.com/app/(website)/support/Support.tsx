1b7597be8b7245a3e3cab9f1998ef60889bc2657
Tarmo Rohelsaar <tarmo.rohelsaar@gmail.com>
2025-03-11 11:52:22 +0200

    Website support copy

--- packages/guvam.com/app/(website)/support/Support.tsx
+++ packages/guvam.com/app/(website)/support/Support.tsx
@@ -8,40 +8,35 @@ export const Support: FC = () => {
   return (
     <section className="Layout-section">
       <div className="Layout-sectionContent">
-        <div className="Grid">
-          <div className="Content ">
-            <div className="Card">
-              <div className="Card-content Content">
-                <h2 className="Content-heading2">Community Support</h2>
-                <p className="Content-text">Become part of our community!</p>
-                <p className="Content-text">
-                  <Link className="Support-link" href="https://github.com/guvam/guvam/discussions">
-                    Talk to the community
-                  </Link>
-                </p>
-                <p className="Content-text">
-                  <Link className="Support-link" href="https://github.com/guvam/guvam/issues">
-                    Report any issues
-                  </Link>
-                </p>
-              </div>
+        <div className="Grid Content">
+          <div className="Card">
+            <div className="Card-content Content">
+              <h2 className="Content-heading2">Community Support</h2>
+              <p className="Content-text">Become part of our community!</p>
+              <p className="Content-text">
+                <Link className="Support-link" href="https://github.com/guvam/guvam/discussions">
+                  Talk to the community
+                </Link>
+              </p>
+              <p className="Content-text">
+                <Link className="Support-link" href="https://github.com/guvam/guvam/issues">
+                  Report any issues
+                </Link>
+              </p>
             </div>
           </div>
-
-          <div>
-            <div className="Card">
-              <div className="Card-content Content">
-                <h2 className="Content-heading2">Professional Support</h2>
-                <p className="Content-text">
-                  Have any questions or need assistance? Send an email, and we&apos;ll get back to
-                  you as soon as possible.
-                </p>
-                <p className="Content-text">
-                  <Link className="Support-link" href="mailto:hello@guvam.com">
-                    <EnvelopeFill /> Talk to us
-                  </Link>
-                </p>
-              </div>
+          <div className="Card">
+            <div className="Card-content Content">
+              <h2 className="Content-heading2">Professional Support</h2>
+              <p className="Content-text">
+                Have any questions or need assistance? Send an email, and we&apos;ll get back to you
+                as soon as possible.
+              </p>
+              <p className="Content-text">
+                <Link className="Support-link" href="mailto:hello@guvam.com">
+                  <EnvelopeFill /> Talk to us
+                </Link>
+              </p>
             </div>
           </div>
         </div>

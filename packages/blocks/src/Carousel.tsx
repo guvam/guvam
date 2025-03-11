ff1ffd97c8b6ada4cb165ef168296c1017a86267
G.Vepkhvadze <56551165+G-Vepkhvadze@users.noreply.github.com>
2025-03-11 11:07:54 +0200

    made the animated carousel variant transitions as requested
    made the nav button function and sync with the autoScroll for the animated variant of the Carousel
    
    Removed all possible uses for Input for now, as it's not needed yet

--- packages/blocks/src/Carousel.tsx
+++ packages/blocks/src/Carousel.tsx
@@ -3,114 +3,183 @@ import Image from "next/image";
 import type { FC } from "react";
 
 export const CarouselImages: FC = () => (
-  <Carousel count={3}>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_2.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_6.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_4.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_5.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_6.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_4.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_2.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_6.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_4.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_5.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_6.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-    <div className="Carousel-trackItem">
-      <Image
-        src="/blocks/carousel/carousel_4.jpg"
-        alt=""
-        width={736}
-        height={414}
-        className="Carousel-trackImage"
-      />
-    </div>
-  </Carousel>
+  <div className="List-stack">
+    <Carousel count={3}>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_2.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_6.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_4.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_5.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_6.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_4.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_2.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_6.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_4.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_5.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_6.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_4.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+      </div>
+    </Carousel>
+    <Carousel count={1} type="animated" autoScroll autoScrollInterval={8000}>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_2.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+        <div className="Carousel-content">
+          <h1 className="Content-heading1">Welcome to Our Website</h1>
+          <p className="Content-titleDescription">Discover amazing features and services.</p>
+        </div>
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_6.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+        <div className="Carousel-content">
+          <h1 className="Content-heading1">This is Fantastic</h1>
+          <p className="Content-titleDescription">Discover amazing features and services.</p>
+        </div>
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_4.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+        <div className="Carousel-content">
+          <h1 className="Content-heading1">And Smooth as can be</h1>
+          <p className="Content-titleDescription">Discover amazing features and services.</p>
+        </div>
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_4.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+        <div className="Carousel-content">
+          <h1 className="Content-heading1">And Smooth as can be</h1>
+          <p className="Content-titleDescription">Discover amazing features and services.</p>
+        </div>
+      </div>
+      <div className="Carousel-trackItem">
+        <Image
+          src="/blocks/carousel/carousel_4.jpg"
+          alt=""
+          width={736}
+          height={414}
+          className="Carousel-trackImage"
+        />
+        <div className="Carousel-content">
+          <h1 className="Content-heading1">And Smooth as can be</h1>
+          <p className="Content-titleDescription">Discover amazing features and services.</p>
+        </div>
+      </div>
+    </Carousel>
+  </div>
 );
--- packages/components/elements/Carousel.tsx
+++ packages/components/elements/Carousel.tsx
@@ -2,42 +2,83 @@
 
 import { classList } from "@guvam/components";
 import type { FC, ReactElement } from "react";
-import { Children, cloneElement, useCallback, useState } from "react";
+import { Children, cloneElement, useCallback, useEffect, useRef, useState } from "react";
 
 interface CarouselProps {
   children: ReactElement<HTMLElement>[];
   count?: number;
   index?: number;
   loop?: boolean;
+  autoScroll?: boolean;
+  autoScrollInterval?: number;
+  type?: "default" | "animated";
 }
 
-export const Carousel: FC<CarouselProps> = ({ children, count = 1, index = 0, loop = false }) => {
+export const Carousel: FC<CarouselProps> = ({
+  children,
+  count = 1,
+  index = 0,
+  loop = false,
+  autoScroll = false,
+  autoScrollInterval = 4000,
+  type = "default",
+}) => {
   const [currentIndex, setCurrentIndex] = useState(index);
+  const maxIndex = children.length - count;
+  const autoScrollRef = useRef<NodeJS.Timeout | null>(null); // Store the interval
+
+  const restartAutoScroll = useCallback(() => {
+    if (autoScroll && autoScrollRef.current) {
+      clearInterval(autoScrollRef.current);
+      autoScrollRef.current = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex >= maxIndex ? 0 : prevIndex + 1));
+      }, autoScrollInterval);
+    }
+  }, [autoScroll, autoScrollInterval, maxIndex]);
 
   const updateIndex = useCallback(
     (i: number) => {
-      let calcIndex = i;
-      const maxIndex = children.length - count;
-
       if (i < 0) {
-        calcIndex = loop ? maxIndex : 0;
+        setCurrentIndex(loop ? maxIndex : 0);
       } else if (i > maxIndex) {
-        calcIndex = loop ? 0 : maxIndex;
+        setCurrentIndex(loop ? 0 : maxIndex);
+      } else {
+        setCurrentIndex(i);
       }
 
-      setCurrentIndex(calcIndex);
+      restartAutoScroll();
     },
-    [children.length, count, loop]
+    [maxIndex, loop, restartAutoScroll]
   );
+
+  useEffect(() => {
+    if (!autoScroll) {
+      return;
+    }
+
+    autoScrollRef.current = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex >= maxIndex ? 0 : prevIndex + 1));
+    }, autoScrollInterval);
+
+    return () => {
+      if (autoScrollRef.current) {
+        clearInterval(autoScrollRef.current);
+      }
+    };
+  }, [autoScroll, autoScrollInterval, maxIndex]);
 
   return (
     <div
-      className="Carousel"
+      className={classList({
+        Carousel: true,
+        "Carousel--type-animated": type === "animated",
+      })}
       style={
         {
           "--Carousel-viewCount": count,
           "--Carousel-slideCount": children.length,
           "--Carousel-currentIndex": currentIndex,
+          "--Carousel-scrollTime": autoScrollInterval + "ms",
         } as never
       }
     >
@@ -47,22 +88,30 @@ export const Carousel: FC<CarouselProps> = ({ children, count = 1, index = 0, lo
             className: classList({
               [element.props.className]: true,
               "Carousel-trackItem--active": currentIndex === i,
+              "Carousel-trackItem--next": type === "animated" && currentIndex + 1 === i,
+              "Carousel-trackItem--previous": type === "animated" && currentIndex - 1 === i,
             }),
           })
         )}
       </div>
-      <button
-        className="Carousel-button Carousel-button--previous"
-        onClick={() => updateIndex(currentIndex - 1)}
-      >
-        &#10094;
-      </button>
-      <button
-        className="Carousel-button Carousel-button--next"
-        onClick={() => updateIndex(currentIndex + 1)}
-      >
-        &#10095;
-      </button>
+
+      {!autoScroll && (
+        <>
+          <button
+            className="Carousel-button Carousel-button--previous"
+            onClick={() => updateIndex(currentIndex - 1)}
+          >
+            &#10094;
+          </button>
+          <button
+            className="Carousel-button Carousel-button--next"
+            onClick={() => updateIndex(currentIndex + 1)}
+          >
+            &#10095;
+          </button>
+        </>
+      )}
+
       <nav className="Carousel-nav">
         {Array(children.length - count + 1)
           .fill(null)
